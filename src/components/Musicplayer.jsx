55ef6edc44630af456a187dc24d8899447df48ff
sarathkp <sarathkpreman@gmail.com>
2026-06-09 14:23:29 +0530

    Fixed bugs and Minor Improvments

--- src/components/Musicplayer.jsx
+++ src/components/Musicplayer.jsx
@@ -38,39 +38,43 @@ export const Musicplayer = ({
         setCurrentTime(audio.currentTime)
     }
 
-    const handleEnded = () => {
-      handleNextSong()
+    // const handleEnded = () => {
+    //   handleNextSong()
 
-    }
+    // }
 
     audio.addEventListener('loadedmetadata', handleLoadedMetaData)
     audio.addEventListener('timeupdate', handleTimeUpdate)
-    audio.addEventListener('ended', handleEnded)
+    // audio.addEventListener('ended', handleEnded)
 
     return () => {
       audio.removeEventListener('loadedmetadata', handleLoadedMetaData)
       audio.removeEventListener('timeupdate', handleTimeUpdate)
-      audio.removeEventListener('ended', handleEnded)
+      // audio.removeEventListener('ended', handleEnded)
     }
   }, [setDuration, setCurrentTime, currentSong, handleNextSong])
 
 
-  useEffect(()=> {
-    const audio = audioRef.current
-    if(!audio) return
+  useEffect(() => {
+  const audio = audioRef.current
 
-    if(isPlaying) {
-      audio.play().catch((error) => console.log(error))
-    } else {
-      audio.pause()
-    }
-  }, [isPlaying])
+  if (!audio) return
+
+  if (isPlaying) {
+    audio.play().catch((error) => {
+      console.log("PLAY FAILED", error)
+    })
+  } else {
+    audio.pause()
+  }
+}, [isPlaying, currentSong])
+
 
 
 
   return (
     <div className="music-player">
-      <audio ref={audioRef} src={currentSong?.url} preload='metadata' crossOrigin='anonymous'/>
+      <audio ref={audioRef} src={currentSong?.url} preload='metadata' crossOrigin='anonymous' onEnded={handleNextSong}/>
 
       <div className="player-info">
         <div className="track-title">
@@ -114,6 +118,15 @@ export const Musicplayer = ({
       <button onClick={handleNextSong}>
         ⏭
       </button>
+    </div>
+
+    <div className='volume-container'>
+      <span className='volume-icon'>🔊</span>
+      <input type="range" min="0" max="1" step="0.01" onChange={(e) => {
+        const audio = audioRef.current
+        if(!audio) return
+        audio.volume = e.target.value
+      }} />
     </div>
 
     </div>
--- src/hooks/useMusic.js
+++ src/hooks/useMusic.js
@@ -1,95 +1,95 @@
 import songs from '../data/songs.js'
-import { useEffect } from 'react'
-
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useMusic = () => {
-    const [allSongs, setAllSongs] = useState(songs)
-    const [currentSong, setCurrentSong] = useState(songs[0])
-    const [currentSongIndex, setCurrentSongIndex] = useState(0)
-    const [currentTime, setCurrentTime] = useState(0)
-    const [duration, setDuration] = useState(0)
-    const [isPlaying, setIsPlaying] = useState()
+  const [allSongs, setAllSongs] = useState(songs)
+  const [currentSongIndex, setCurrentSongIndex] = useState(0)
 
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(0)
 
-    useEffect(() => {
+  const [isPlaying, setIsPlaying] = useState()
+
+  const currentSong = allSongs[currentSongIndex]
+
+  useEffect(() => {
     const loadSongs = async () => {
-    const songsWithDuration = await Promise.all(
-      songs.map((song) => {
-        return new Promise((resolve) => {
-          const audio = new Audio(song.url);
+      const songsWithDuration = await Promise.all(
+        songs.map((song) => {
+          return new Promise((resolve) => {
+            const audio = new Audio(song.url)
 
-          audio.addEventListener("loadedmetadata", () => {
-            resolve({
-              ...song,
-              duration: audio.duration,
-            });
-                });
-                });
-                })
-             );
+            audio.addEventListener('loadedmetadata', () => {
+              resolve({
+                ...song,
+                duration: audio.duration,
+              })
+            })
+          })
+        })
+      )
 
-            setAllSongs(songsWithDuration);
-            };
-
-        loadSongs();
-    }, []);
-
-
-    const handlePlaySong = (song, index) => {
-        setCurrentSong(song)
-        setCurrentSongIndex(index)
-        setDuration(song.duration)
+      setAllSongs(songsWithDuration)
     }
-    const formatTime = (time) => {
-        if (typeof time !== "number" || isNaN(time)) {
-        return "0:00"
-    }
+
+    loadSongs()
+  }, [])
+
+  const handlePlaySong = (song, index) => {
+  console.log("CLICKED", song.title)
+
+  setCurrentSongIndex(index)
+  setDuration(song.duration || 0)
+  setCurrentTime(0)
+  setIsPlaying(true)
+}
+
+  const play = () => setIsPlaying(true)
+
+  const pause = () => setIsPlaying(false)
+
+  const handleNextSong = () => {
+    setCurrentSongIndex((prev) => (prev + 1) % allSongs.length)
+
+    setCurrentTime(0)
+    //setIsPlaying(false)
+  }
+
+  const handlePreviousSong = () => {
+    setCurrentSongIndex((prev) =>
+      prev === 0 ? allSongs.length - 1 : prev - 1
+    )
+
+    setCurrentTime(0)
+      //setIsPlaying(false)
     
+  }
+
+  const formatTime = (time) => {
+    if (typeof time !== 'number' || isNaN(time)) {
+      return '0:00'
+    }
 
     const minutes = Math.floor(time / 60)
     const seconds = Math.floor(time % 60)
 
-    return `${minutes}:${seconds.toString().padStart(2, "0")}`
-    }
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
 
-
-    const play = () => setIsPlaying(true)
-    const pause = () => setIsPlaying(false)
-
-
-    const handleNextSong = () => {
-        setCurrentSongIndex((prev)=> {
-            const nextIndex = (prev + 1 ) % allSongs.length;
-            setCurrentSong(allSongs[nextIndex]);
-            return nextIndex
-        })
-        setIsPlaying(false)
-    }   
-
-    const handlePreviousSong = () => {
-        setCurrentSongIndex((prev)=> {
-            const prevIndex = prev === 0 ? allSongs.length - 1 : prev - 1;
-            setCurrentSong(allSongs[prevIndex]);
-            return prevIndex
-        })
-        setIsPlaying(false)
-    }
-
-    
-    return { allSongs, 
-        handlePlaySong, 
-        currentSongIndex, 
-        currentSong, 
-        currentTime, 
-        setCurrentTime, 
-        formatTime, 
-        duration, 
-        setDuration,
-        handleNextSong,
-        handlePreviousSong,
-        play,
-        pause, 
-        isPlaying
-    } 
+  return {
+    allSongs,
+    currentSong,
+    currentSongIndex,
+    currentTime,
+    setCurrentTime,
+    duration,
+    setDuration,
+    isPlaying,
+    play,
+    pause,
+    handlePlaySong,
+    handleNextSong,
+    handlePreviousSong,
+    formatTime,
+  }
 }
